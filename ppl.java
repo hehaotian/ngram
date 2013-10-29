277b0dcb1f0f28a576e5c6ebc4b529903273275a
Haotian He <haotianh@uw.edu>
2013-10-28 19:20:30 -0700

    something changed

--- ppl.java
+++ ppl.java
@@ -7,13 +7,16 @@ public class ppl {
    public static Map<String, String[]> grams1 = new HashMap<String, String[]>();
    public static Map<String, String[]> grams2 = new HashMap<String, String[]>();
    public static Map<String, String[]> grams3 = new HashMap<String, String[]>();
+   public static double l1;
+   public static double l2;
+   public static double l3;
 
    public static void main(String[] args) throws IOException {
    
       Scanner lm = new Scanner(new File(args[0]));
-      double l1 = Double.parseDouble(args[1]);
-      double l2 = Double.parseDouble(args[2]);
-      double l3 = Double.parseDouble(args[3]);
+      l1 = Double.parseDouble(args[1]);
+      l2 = Double.parseDouble(args[2]);
+      l3 = Double.parseDouble(args[3]);
       Scanner test = new Scanner(new File(args[4]));
       PrintStream ps = new PrintStream(args[5]);
       
@@ -63,7 +66,7 @@ public class ppl {
             word_num += sent_word;
             System.out.println("Sent #" + sent_num + ": " + line);
             calculate(tokens, sent_num);
-            System.out.println("1 sentence, " + sent_word + "words");
+            System.out.println("1 sentence, " + sent_word + " words");
             System.out.println();
          }
       }
@@ -73,8 +76,47 @@ public class ppl {
    }
    
    public static void calculate(String[] tokens, int sent_num) {
-   
       
+      ArrayList<Double> probs = new ArrayList<Double>();
+      ArrayList<Double> lgprobs = new ArrayList<Double>();
+      
+      String token = "";
+      String check = "";
+      
+      double prob = 1.0;
+      
+      // First bigrams:
+      token = tokens[0] + " " + tokens[1];
+      if (grams2.get(token) != null && grams1.get(tokens[1]) != null)
+         prob = l2 * Double.parseDouble(grams2.get(token)[2]) + l1 * Double.parseDouble(grams1.get(tokens[1])[2]);
+      else prob = 100.0;
+      probs.add(prob);
+      lgprobs.add(Math.log10(prob));
+      
+      // Other trigrams:
+      for (int i = 2; i < tokens.length; i ++) {
+         token = tokens[i - 2] + " " + tokens[i - 1] + " " + tokens[i];
+         if (grams3.get(token) != null && grams2.get(tokens[i - 1] + " " + tokens[i]) != null && grams1.get(tokens[i]) != null)
+            prob = l3 * Double.parseDouble(grams3.get(token)[2]) + l2 * Double.parseDouble(grams2.get(tokens[i - 1] + " " + tokens[i])[2]) + l1 * Double.parseDouble(grams1.get(tokens[i])[2]);
+         else prob = 100.0;
+         probs.add(prob);
+         lgprobs.add(Math.log10(prob));
+      }
+      
+      print(probs, tokens);
+   }
    
+   public static void print(ArrayList<Double> lgprobs, String[] tokens) {
+      
+      for (int i = 1; i < tokens.length; i ++) {
+         if (i == 1) {
+            System.out.print(i + ": lg P(" + tokens[i] + " | " + tokens[i - 1] + ") = ");
+            System.out.println(lgprobs.get(0));
+         } else {
+            System.out.print(i + ": lg P(" + tokens[i] + " | " + tokens[i - 2] +  " " + tokens[i - 1] + ") = ");
+            System.out.println(lgprobs.get(i - 1));
+         }
+      }
+      
    }
 }
